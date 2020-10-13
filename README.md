d2e8a3505d51917c5502eeb7ab54ec328f042f5e
KaladaJumbo <wkalada1@gmail.com>
2020-10-13 19:07:29 -0400

    final commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
   - Fetch and display a row for each product
   - When you submit the form, make a new product in the DB and a new row in the frontend
   - When you click the 'X the product is removed from the front and backEnd
+
--- script.js
+++ script.js
@@ -56,3 +56,103 @@
 
 
 // }
+
+
+
+
+const URL = "http://localhost:3000/products/"
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetching();
+    formListener();
+})
+
+function fetching(url=URL) {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => data.forEach(element => createProduct(element)))
+}
+
+function createProduct(params) {
+    let row = document.createElement("tr");
+    //row.id = `id${params.id}`
+
+    //table data 
+    let name = document.createElement("td");
+    let price = document.createElement("td");
+    let quantity = document.createElement("td");
+    let deletebutton = document.createElement("td")
+
+    //adding data to the respective tr's
+    name.innerText = params.name
+    price.innerText = params.price
+    quantity.innerText = params.quantity
+    deletebutton.innerText = "X"
+
+    //find the table body 
+    let table = document.querySelector(".table")
+    body = table.children[1];
+
+    //appending 
+    row.appendChild(name);
+    row.appendChild(price);
+    row.appendChild(quantity);
+    row.appendChild(deletebutton)
+
+    body.appendChild(row);
+
+    //listener 
+    deletebutton.addEventListener("click", () => {
+        row.remove()
+        removeFromDB(params)
+    })
+
+}
+
+function removeFromDB(params) {
+    let showUrl = `${URL}/${params.id}`
+    meta = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+    fetch(showUrl, meta)
+        .then(res => {console.log("success")})
+        .catch(failure => {console.log(failure)})
+}
+
+function formListener() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        postNewProd(e)
+    })
+
+}
+
+function postNewProd(e) {
+    
+    let newProduct = {
+        name: e.target[0].value,
+        price: e.target[1].value,
+        quantity: e.target[2].value
+    }
+
+    let meta = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newProduct)
+    }
+
+    fetch(URL, meta)
+        .then(res => res.json())
+        .then(createProduct(newProduct))
+        .catch(error => console.log(error))
+
+        
+}
